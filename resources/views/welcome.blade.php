4a85823e63030773be4c9a775c68a24e77c0c8e2
marc.k28@hotmail.com <marc.k28@hotmail.com>
2020-05-20 09:46:59 -0500

    Cambio titulo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    CAMBIOS
                 </div>
 
                 <div class="links">
